6cb161c4acc4a9d16217cd57bb120c4c268c3da4
Totallysql <Totallysql@localhost>
2013-04-27 23:53:39 +0100

    Initial Load

---
+++ SQLCore/SqlCoreStub.cs
@@ -0,0 +1,50 @@
+﻿#define SqlCore
+#if SqlStub
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLCore
+{
+
+    public class SqlCore
+    {
+        public static bool IsEnabled
+        {
+            get { return false; }
+        }
+    }
+
+    public class ThreadSafeDictionary<K, V>
+    {
+
+        public void Add(K key, V value)
+        {
+            throw new NotImplementedException("The installed version of SQLCore is non-functional - for Safe mode deployment only");
+        }
+
+        public V this[K key]
+        {
+            get
+            {
+                throw new NotImplementedException("The installed version of SQLCore is non-functional - for Safe mode deployment only");
+            }
+
+            set
+            {
+                throw new NotImplementedException("The installed version of SQLCore is non-functional - for Safe mode deployment only");
+            }
+        }
+
+        public bool Remove(K key)
+        {
+            throw new NotImplementedException("The installed version of SQLCore is non-functional - for Safe mode deployment only");
+        }
+
+        public bool ContainsKey(K key)
+        {
+            throw new NotImplementedException("The installed version of SQLCore is non-functional - for Safe mode deployment only");
+        }
+    }
+}
+#endif
